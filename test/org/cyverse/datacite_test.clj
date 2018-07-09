432ff0e561b6b901d15a3cf8cdbf332a4545d6d6
Sarah Roberts <sarahr@cyverse.org>
2018-07-09 11:24:36 -0700

    CORE-9253: added unit tests for DataCite file generation with missing fields

--- test/org/cyverse/datacite_test.clj
+++ test/org/cyverse/datacite_test.clj
@@ -7,18 +7,55 @@
   (:import [java.net URL]))
 
 (def ^:private min-attrs
-  (build-attributes {"Identifier"             "the-identifier"
-                     "identifierType"           "DOI"
-                     "datacite.creator"         "Nobody Inparticular"
-                     "creatorAffiliation"       "The University of Nowhere"
-                     "datacite.title"           "A Very Important Data Set"
-                     "datacite.publisher"       "CyVerse Data Commons"
-                     "datacite.publicationyear" "2018"
-                     "datacite.resourcetype"    "Data Set"}))
+  {"Identifier"               "the-identifier"
+   "identifierType"           "DOI"
+   "datacite.creator"         "Nobody Inparticular"
+   "creatorAffiliation"       "The University of Nowhere"
+   "datacite.title"           "A Very Important Data Set"
+   "datacite.publisher"       "CyVerse Data Commons"
+   "datacite.publicationyear" "2018"
+   "datacite.resourcetype"    "Data Set"})
 
 (def ^:private schema-url
   (URL. "https://schema.datacite.org/meta/kernel-4.1/metadata.xsd"))
 
+(defn- test-missing-field [field-name]
+  (is (thrown-with-msg? clojure.lang.ExceptionInfo
+                        (re-pattern (str "Missing required attributes: " field-name))
+                        (build-datacite (build-attributes (dissoc min-attrs field-name))))))
+
 (deftest test-minimal
   (testing "Minimal DataCite file."
-    (test-xml "datacite/minimal.xml" (build-datacite min-attrs) schema-url)))
+    (test-xml "datacite/minimal.xml" (build-datacite (build-attributes min-attrs)) schema-url)))
+
+(deftest test-missing-identifier
+  (testing "DataCite file generation with missing identifier."
+    (test-missing-field "Identifier")))
+
+(deftest test-missing-identifier-type
+  (testing "DataCite file generation with missing identifier type."
+    (test-missing-field "identifierType")))
+
+(deftest test-missing-creator
+  (testing "DataCite file generation with missing creator."
+    (test-missing-field "datacite.creator")))
+
+(deftest test-missing-creator-affiliation
+  (testing "DataCite file generation with missing creator affiliation."
+    (test-missing-field "creatorAffiliation")))
+
+(deftest test-missing-title
+  (testing "DataCite file generation with missing title."
+    (test-missing-field "datacite.title")))
+
+(deftest test-missing-publisher
+  (testing "DataCite file generation with missing publisher."
+    (test-missing-field "datacite.publisher")))
+
+(deftest test-missing-publication-year
+  (testing "DataCite file generation with missing publication year."
+    (test-missing-field "datacite.publicationyear")))
+
+(deftest test-missing-resource-type
+  (testing "DataCite file generation with missing resource type."
+    (test-missing-field "datacite.resourcetype")))
