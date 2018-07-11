b1450e7f1f4fe9422b9c58e6fb65e37078af90e8
Sarah Roberts <sarahr@cyverse.org>
2018-07-11 13:22:11 -0700

    CORE-9253: switched the version to 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/metadata-files "0.1.0-SNAPSHOT"
+(defproject org.cyverse/metadata-files "1.0.0"
   :description "Library for generating metadata files."
   :url "https://github.com/cyverse-de/metadata-files"
   :license {:name "BSD"
