a970191ddae41626659a7e28aec3f9d6e2d7b6fe
Sarah Roberts <sarahr@cyverse.org>
2019-02-28 17:31:46 -0700

    CORE-104: update some dependencies

--- project.clj
+++ project.clj
@@ -5,8 +5,9 @@
             :url "http://www.cyverse.org/license"}
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
-  :plugins [[jonase/eastwood "0.2.3"]
+  :plugins [[jonase/eastwood "0.3.5"]
             [test2junit "1.2.2"]]
+  :eastwood {:exclude-linters [:implicit-dependencies]}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/data.xml "0.2.0-alpha5"]]
+                 [org.clojure/data.xml "0.2.0-alpha6"]]
   :profiles {:dev {:resource-paths ["test-resources"]}})
