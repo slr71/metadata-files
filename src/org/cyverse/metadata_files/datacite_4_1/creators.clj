c159e9b00d62ec1cc9a18454c3453a00b7556be1
Sarah Roberts <sarahr@cyverse.org>
2019-03-06 09:40:39 -0700

    CORE-104: removed an unused reference to clojure.tools.logging

--- src/org/cyverse/metadata_files/datacite_4_1/creators.clj
+++ src/org/cyverse/metadata_files/datacite_4_1/creators.clj
@@ -3,7 +3,6 @@
         [clojure.data.xml :only [element]]
         [org.cyverse.metadata-files.datacite-4-1.namespaces :only [alias-uris]])
   (:require [clojure.string :as string]
-            [clojure.tools.logging :as log]
             [org.cyverse.metadata-files :as mdf]
             [org.cyverse.metadata-files.util :as util]))
 
