8d810d54249dab9c2cfb1fec5227c8f0506cbc6c
Sarah Roberts <sarahr@cyverse.org>
2019-03-06 16:24:35 -0700

    CORE-104: fixed the docstring for the get-location method

--- src/org/cyverse/metadata_files.clj
+++ src/org/cyverse/metadata_files.clj
@@ -10,7 +10,7 @@
   (min-occurs [_] "Returns the minimum number of occurrences of the element.")
   (max-occurs [_] "Returns the maximum number of occurrences of the element.")
   (child-element-factories [_] "Returns the list of element factories for child elements.")
-  (get-location [_] "Returns the location of the current element in the document.")
+  (get-location [_] "Returns the location of the current attribute in the metadata template hierarchy.")
   (validate [_ attribute] "Validates the attributes used to generate the element.")
   (generate-nested [_ attribute] "Generates the element."))
 
