da730b670c86a82ae2d32998197ede2c83373558
Paul Sarando <psarando@cyverse.org>
2019-04-23 15:40:54 -0700

    Version 1.0.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/metadata-files "1.0.3-SNAPSHOT"
+(defproject org.cyverse/metadata-files "1.0.3"
   :description "Library for generating metadata files."
   :url "https://github.com/cyverse-de/metadata-files"
   :license {:name "BSD"
