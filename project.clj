2d4599503d51db443e6e68cd18a5bb3921f9d2a0
Paul Sarando <psarando@cyverse.org>
2023-12-06 16:48:17 -0700

    Version 2.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/metadata-files "2.1.0"
+(defproject org.cyverse/metadata-files "2.1.1-SNAPSHOT"
   :description "Library for generating metadata files."
   :url "https://github.com/cyverse-de/metadata-files"
   :license {:name "BSD"
